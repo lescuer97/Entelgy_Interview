ceeb6754e503a57443c88fd7555496ddc51dbdac
lescuer97 <leoescuer@protonmail.com>
2021-02-25 19:08:23 +0100

    added utility functions
    Signed-off-by: lescuer97 <leoescuer@protonmail.com>

--- utils/utilFunction.js
+++ utils/utilFunction.js
@@ -1,6 +1,5 @@
 exports.range = (array, query) => {
   let result = [];
-
   let limit;
   if (array.length <= query) {
     limit = array.length;
@@ -21,3 +20,17 @@ exports.range = (array, query) => {
   }
   return result;
 };
+
+// searches either an id of client in the client array or the id of a policy in the policy array
+exports.searcher = (array, query) => {
+  let results = array.filter((obj) => obj.id === query);
+  return results;
+};
+
+// searches the policy  array for the id of the client and returns it in the array if equal
+exports.searchClientIdinPolicy = (array, query) => {
+  let results = array.filter((obj) => obj.clientId === query);
+  return results;
+};
+// TODO WRITE FUNCTION
+exports.filterForPolicy = (array) => {};
