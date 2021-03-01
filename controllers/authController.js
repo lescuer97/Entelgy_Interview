c8b009be19cddb79ad560d994292b01dafcb4068
lescuer97 <leoescuer@protonmail.com>
2021-03-01 14:58:51 +0100

    parts of authentication
    Signed-off-by: lescuer97 <leoescuer@protonmail.com>

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,16 @@
 const axios = require("axios").default;
+const jwt = require("jsonwebtoken");
 const NodeCache = require("node-cache");
 
 const catchAsync = require("../utils/catchAsync");
 var authCache = new NodeCache();
+
+const signToken = (ids) => {
+  return jwt.sign({ ids }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
 // login to the api client
 exports.insuranceApiLogin = catchAsync(async (req, res, next) => {
   const token = authCache.get("token");
@@ -31,4 +39,37 @@ exports.insuranceApiLogin = catchAsync(async (req, res, next) => {
     req.auth = token;
     next();
   }
+});
+
+exports.userLogin = (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1) Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError("Please provide email and password!", 401));
+  }
+};
+
+exports.protectUserLogin = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check of it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError("You are not logged in! Please log in to get access.", 401)
+    );
+  }
+
+  // 2) Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // TODO MAKE A SEARCH OF THE USER TO GET ITS DATA AND VERIFY THE USER STILL EXISTS
+
+  // TODO  CREATE REQ.USER TO TRANSFER FOR THE CONTROLLERS
 });
