02a66d0e23ca6da5c55813171e7c9f86a70e0a04
lescuer97 <leoescuer@protonmail.com>
2021-03-04 23:40:01 +0100

    added rudimentary caching
    Signed-off-by: lescuer97 <leoescuer@protonmail.com>

--- controllers/clientsController.js
+++ controllers/clientsController.js
@@ -1,27 +1,52 @@
 const axios = require("axios");
 const catchAsync = require("../utils/catchAsync");
+const NodeCache = require("node-cache");
 const {
   idCompare,
   searchClientIdinPolicyArray,
   addPoliciesToClients,
 } = require("../utils/utilFunction");
 
+// var clientCache = new NodeCache();
+
+var clientCache = new NodeCache();
+
 // grabs all the clients from the api
 exports.clientAPIRequest = catchAsync(async (req, res, next) => {
+  // checks if etag exists else returns empty string
+  const cache = (function(a) {
+    if (a) {
+      return a;
+    } else {
+      return { etag: "" };
+    }
+  })(clientCache.get("client"));
+
   const query = await axios({
     method: "get",
     url: "https://dare-nodejs-assessment.herokuapp.com/api/clients",
     headers: {
       authorization: req.auth,
-      // "If-None-Match": req.headers["If-None-Match"],
+      "If-None-Match": cache.etag,
     },
     data: {
       client_id: req.body.client_id,
       client_secret: req.body.client_secret,
     },
-  }).then((res) => {
-    return res.data;
-  });
+  })
+    .then((res) => {
+      clientCache.set("client", { etag: res.headers.etag, values: res.data });
+      // console.log(res.headers);
+      return res.data;
+    })
+    .catch((err) => {
+      // this checks that the response was 304 and if it was it sends the cached values to the next controllers
+      if (err.response.status && clientCache.get("client")) {
+        return clientCache.get("client").values;
+      } else {
+        return next(err);
+      }
+    });
 
   req.clients = query;
   next();
--- controllers/policyControllers.js
+++ controllers/policyControllers.js
@@ -1,23 +1,46 @@
 const axios = require("axios");
+const NodeCache = require("node-cache");
 const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 const { range, idCompare } = require("../utils/utilFunction");
 
+var policyCache = new NodeCache();
 //grabs the policies from the client
 exports.policyAPIRequest = catchAsync(async (req, res, next) => {
+  // checks if etag exists else returns empty string
+  const cache = (function(a) {
+    if (a) {
+      return a;
+    } else {
+      return { etag: "" };
+    }
+  })(policyCache.get("policy"));
+
   const query = await axios({
     method: "get",
     url: "https://dare-nodejs-assessment.herokuapp.com/api/policies",
     headers: {
       authorization: req.auth,
+      "If-None-Match": cache.etag,
     },
     data: {
       client_id: req.body.client_id,
       client_secret: req.body.client_secret,
     },
-  }).then((res) => {
-    return res.data;
-  });
+  })
+    .then((res) => {
+      policyCache.set("policy", { etag: res.headers.etag, values: res.data });
+      return res.data;
+    })
+    .catch((err) => {
+      // this checks that the response was 304 and if it was it sends the cached values to the next controllers
+      if (err.response.status && policyCache.get("policy")) {
+        return policyCache.get("policy").values;
+      } else {
+        return next(err);
+      }
+    });
+
   req.policy = query;
   next();
 });
